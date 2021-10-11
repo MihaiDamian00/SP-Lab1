faf00a14e8580d7d4314af907d58b0f9e39bcf80
Mihai Damian <76659068+mihaidam@users.noreply.github.com>
2021-10-11 12:23:37 +0300

    Added Book, Author and Chapter classes, edited Main to test them

---
+++ src/main/java/Book.java
@@ -0,0 +1,36 @@
+import java.util.LinkedList;
+import java.util.List;
+
+public class Book {
+    String title;
+    List<Author> AuthorList = new LinkedList<>();
+    List<Chapter> ChapterList = new LinkedList<>();
+
+    public Book(String title) {
+        this.title = title;
+//        this.AuthorList = new LinkedList<Author>();
+    }
+
+    public void addAuthor(Author author) {
+        this.AuthorList.add(author);
+    }
+
+    public int createChapter(String chapterName) {
+        Chapter chap = new Chapter(chapterName);
+        this.ChapterList.add(chap);
+
+        return this.ChapterList.indexOf(chap);
+    }
+
+    public Chapter getChapter(int index) {
+        return this.ChapterList.get(index);
+    }
+
+    public void print() {
+        System.out.println("Title: " + this.title);
+        System.out.println("Author(s): ");
+        for (Author aut : AuthorList) {
+            aut.print();
+        }
+    }
+}
---
+++ src/main/java/Chapter.java
@@ -0,0 +1,11 @@
+public class Chapter {
+    String name;
+
+    public Chapter(String name) {
+        this.name = name;
+    }
+
+    public void print(){
+        System.out.println(this.name);
+    }
+}
