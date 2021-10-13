40a09e439fb3a3fc0b9118733cdae47c80612f84
Mihai Damian <76659068+mihaidam@users.noreply.github.com>
2021-10-13 17:20:51 +0300

    Added Element interface, made SubChapter and elements of it implement the interface

--- src/main/java/Table.java
+++ src/main/java/Table.java
@@ -1,11 +1,26 @@
-public class Table {
+public class Table implements Element {
     String title;
 
     public Table(String title) {
         this.title = title;
     }
 
+    @Override
+    public void createNewParagraph(String para) {
+
+    }
+
+    @Override
+    public void createNewImage(String img) {
+
+    }
+
+    @Override
+    public void createNewTable(String table) {
+
+    }
+
     public void print() {
-        System.out.println(this.title);
+        System.out.println("Table with Title: " + this.title);
     }
 }
