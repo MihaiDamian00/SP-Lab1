9cd7292414000c2af3bd3cb10e3050bf68d514f2
Mihai Damian <76659068+mihaidam@users.noreply.github.com>
2021-11-22 12:12:18 +0200

    Added AlignStrategy interface and classes the implement it

--- src/main/java/Paragraph.java
+++ src/main/java/Paragraph.java
@@ -2,7 +2,8 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class Paragraph extends Element {
-    String text;
+    private String text;
+    private AlignStrategy textAlignment;
 
     public Paragraph(String text) {
         this.text = text;
@@ -23,5 +24,22 @@ public class Paragraph extends Element {
     @Override
     public Element get(int num) {
         return null;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public void setAlignStrategy(AlignStrategy alst) {
+        this.textAlignment = alst;
+        setText(alst.render(this.text));
+    }
+
+    public AlignStrategy getTextAlignment() {
+        return textAlignment;
+    }
+
+    public String getText() {
+        return text;
     }
 }
